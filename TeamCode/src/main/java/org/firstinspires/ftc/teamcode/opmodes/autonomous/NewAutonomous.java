e697f8ce855a5c9aa4f59c6d1fb9979ddc0a78c1
Alicelavander <alicelavander.dev@gmail.com>
2023-10-03 17:09:55 -0400

    worked on new Autonomous navigation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/NewAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/NewAutonomous.java
@@ -1,14 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.components.Arm;
 import org.firstinspires.ftc.teamcode.components.Chassis;
 import org.firstinspires.ftc.teamcode.components.NewVision;
-import org.firstinspires.ftc.teamcode.components.Vision;
+import org.firstinspires.ftc.teamcode.components.old.Vision;
 import org.firstinspires.ftc.teamcode.components.lib.drive.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.utility.RobotConfig;
 import org.firstinspires.ftc.teamcode.utility.teaminfo.InitialSide;
@@ -16,36 +14,37 @@ import org.firstinspires.ftc.teamcode.utility.teaminfo.TeamColor;
 
 @Autonomous(name="2023-24 CenterStage")
 public class NewAutonomous extends LinearOpMode {
-    enum State {
-        START,
-        TRAJ_1,
-        LIFT_ARM,
-        GO_TO_POLE,
-        SCORE_CONE,
-        PARK,
-        WAIT
-    }
-    State currentState = State.WAIT;
-
     //configuration for Blue Alliance Far Side (closer to the drone landing zone)
     Pose2d startPose = new Pose2d(-60, -36, Math.toRadians(0));
     Pose2d backDrop = new Pose2d(-36, 50, Math.toRadians(90));
     Pose2d stack1 = new Pose2d(-36, -50, Math.toRadians(-90));
-
-    boolean done = false;
+    Pose2d park = new Pose2d(-36, -10, Math.toRadians(-90));
 
     @Override
     public void runOpMode() {
         Chassis chassis = new Chassis(hardwareMap);
         NewVision vision = new NewVision(hardwareMap);
 
+        //.waitSeconds
         TrajectorySequence startToBackDrop = chassis.trajectorySequenceBuilder(startPose)
                 .strafeLeft(48)
                 .lineToLinearHeading(backDrop)
-                .build();
-
-        TrajectorySequence backDropToStack = chassis.trajectorySequenceBuilder(backDrop)
+                .addDisplacementMarker(() -> {
+                    //adjust location to AprilTag
+                    //place pixels on backdrop
+                })
                 .lineToLinearHeading(stack1)
+                .addDisplacementMarker(() -> {
+                    //pickup pixels from stack
+                })
+                .lineToLinearHeading(backDrop)
+                .addDisplacementMarker(() -> {
+                    //place pixels on backdrop
+                })
+                .lineToLinearHeading(park)
+                .addDisplacementMarker(() -> {
+                    //park
+                })
                 .build();
 
         while (!isStarted() && !isStopRequested()) {
@@ -59,56 +58,10 @@ public class NewAutonomous extends LinearOpMode {
 
             telemetry.addData("Team color", RobotConfig.teamColor);
             telemetry.addData("Initial side", RobotConfig.initialSide);
+            telemetry.addData("Reading indicator", null);
             telemetry.update();
 
             sleep(20);
-        }
-
-        chassis.setPoseEstimate(startPose);
-        currentState = State.START;
-        //TODO: arm.closeGripper();
-
-        while(opModeIsActive() && !done) {
-            switch (currentState){
-                case START:
-                    if (!chassis.isBusy()) {
-                        currentState = State.TRAJ_1;
-//                        chassis.followTrajectorySequenceAsync(traj1);
-                    }
-                    break;
-                case TRAJ_1:
-                    if (!chassis.isBusy()) {
-                        currentState = State.LIFT_ARM;
-                        //TODO: arm.runToPosition(arm.highJunction);
-                    }
-                    break;
-                case LIFT_ARM:
-                    if (!chassis.isBusy()){
-                        currentState = State.GO_TO_POLE;
-//                        chassis.followTrajectorySequenceAsync(traj2);
-                    }
-                    break;
-                case GO_TO_POLE:
-                    if (!chassis.isBusy()){
-                        currentState = State.SCORE_CONE;
-                        //TODO: arm.fall();
-                        //      arm.openGripper();
-                    }
-                case SCORE_CONE:
-                    if (!chassis.isBusy()){
-                        currentState = State.PARK;
-//                        chassis.followTrajectorySequenceAsync(traj3);
-                    }
-                    break;
-                case PARK:
-                    if (!chassis.isBusy()){
-                        currentState = State.WAIT;
-//                        chassis.followTrajectorySequenceAsync(traj3);
-                    }
-                    break;
-                case WAIT:
-                    break;
-            }
         }
     }
 
