12fc08e05b3a35cf622d34410b2155ac7c4cda80
Alicelavander <alicelavander.dev@gmail.com>
2023-10-24 15:55:05 -0400

    implemented drone shooting mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/NewDrone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/NewDrone.java
@@ -1,49 +1,36 @@
 package org.firstinspires.ftc.teamcode.components;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.utility.RobotConfig;
 
-import java.util.Arrays;
-import java.util.List;
 
 //mahika
 public class NewDrone {
-
-    private List<DcMotor> arms;
-    //initialize sensors & motors in public
     public Servo launchDrone;
     public Servo positionDrone;
-
-    //enum for state (servo 1: home, launchPos; servo 2: firstReleasePos, secondReleasePos)
-    public enum droneServo1state {home,launchPos, none};
-    public enum droneServo2state {firstReleasePos,secondReleasePos, none};
-
-    public droneServo1state currentState  = droneServo1state.none;
-    public droneServo2state currentState2  = droneServo2state.none;
 
     //constructor
     public NewDrone(HardwareMap hardwareMap){
         this.launchDrone = hardwareMap.get(Servo.class, RobotConfig.launchDrone);
         this.positionDrone = hardwareMap.get(Servo.class, RobotConfig.positionDrone);
-
+        launchDrone.setPosition(0.65);
+        positionDrone.setPosition(0.25);
     }
 
-    //public (or private) methods
-    public void homePosition() {}
-    public void launchPosition(){}
-    public void firstRelease() {}
-    public void secondRelease() {}
+    public void home(){
+        launchDrone.setPosition(0.65);
+        positionDrone.setPosition(0.25);
+    }
 
+    public void prepareLaunch(){
+        launchDrone.setPosition(0.65);
+        positionDrone.setPosition(0.5);
+    }
 
-
-    public void runToPosition(int position) {}
-
-    public int getCurrentPosition(){return 0;}
-    public int getTargetPosition(){return 0;}
-    public double getPower(){return 0.0;}
-
+    public void launch(){
+        launchDrone.setPosition(0.85);
+        positionDrone.setPosition(0.5);
+    }
 }
