5243ae001b1ad2ad029434f43bc42dc2e0602dfe
saggarwal25 <91703713+saggarwal25@users.noreply.github.com>
2023-10-25 13:50:15 -0400

    teleop arm fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/FirstAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/FirstAutonomous.java
@@ -7,6 +7,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.components.Chassis;
 import org.firstinspires.ftc.teamcode.components.NewArm;
 import org.firstinspires.ftc.teamcode.components.NewVision;
+import org.firstinspires.ftc.teamcode.components.NewDrone;
+
 import org.firstinspires.ftc.teamcode.utility.RobotConfig;
 import org.firstinspires.ftc.teamcode.utility.teaminfo.InitialSide;
 import org.firstinspires.ftc.teamcode.utility.teaminfo.TeamColor;
@@ -27,6 +29,7 @@ public class FirstAutonomous extends LinearOpMode {
         Chassis chassis = new Chassis(hardwareMap);
         NewArm arm = new NewArm(hardwareMap);
         NewVision vision = new NewVision(hardwareMap);
+        NewDrone drone = new NewDrone(hardwareMap);
 
 
         //open claw for 18-inch restriction
