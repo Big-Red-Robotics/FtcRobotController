37943bf2c438da82c39095834e3a452bcd6543f5
Alicelavander <alicelavander.dev@gmail.com>
2024-01-17 13:34:09 -0500

    light test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/LightTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmodes.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.utility.RobotConfig;
+
+@TeleOp
+public class LightTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        //initialize components
+        DcMotor light = hardwareMap.get(DcMotor.class, "light");
+
+        telemetry.addLine("waiting to start!");
+        telemetry.update();
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            if(gamepad1.a) light.setPower(-0.5);
+            if(gamepad1.b) light.setPower(0.0);
+
+            telemetry.addData("light power", light.getPower());
+            telemetry.update();
+        }
+    }
+}
