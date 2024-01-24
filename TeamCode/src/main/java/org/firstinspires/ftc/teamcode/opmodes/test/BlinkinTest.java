d2187bf401394028dc12ff68cbbd8a5b9e25e337
Alicelavander <alicelavander.dev@gmail.com>
2024-01-24 14:08:19 -0500

    fix blinkin test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/BlinkinTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/BlinkinTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.utility.RobotConfig;
 
@@ -15,14 +16,17 @@ public class BlinkinTest extends LinearOpMode {
         //initialize components
         RevBlinkinLedDriver blinkinRight = hardwareMap.get(RevBlinkinLedDriver.class, RobotConfig.blinkinRight);
         RevBlinkinLedDriver blinkinLeft = hardwareMap.get(RevBlinkinLedDriver.class, RobotConfig.blinkinLeft);
+        DcMotor blinkinPower = hardwareMap.get(DcMotor.class, "blinkinPower");
 
-        RevBlinkinLedDriver.BlinkinPattern pattern = RevBlinkinLedDriver.BlinkinPattern.BEATS_PER_MINUTE_PARTY_PALETTE;
+        RevBlinkinLedDriver.BlinkinPattern pattern = RevBlinkinLedDriver.BlinkinPattern.RAINBOW_RAINBOW_PALETTE;
         blinkinRight.setPattern(pattern);
         blinkinLeft.setPattern(pattern);
 
         telemetry.addLine("waiting to start!");
         telemetry.update();
         waitForStart();
+
+        blinkinPower.setPower(1.0);
 
         while(opModeIsActive()) {
             //move components and log data
